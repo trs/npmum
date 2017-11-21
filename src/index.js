03d86a70fe142c0aea21184a18794aefccc62151
Tyler Stewart <tylers490@gmail.com>
2017-11-21 09:05:14 -0700

    feat: commander setup

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ const {version} = require('../package.json');
 
 const {ls} = require('./commands/ls');
 const {use} = require('./commands/use');
-const {login} = require('./commands/login');
+const {add} = require('./commands/add');
 
 function setup(argv) {
   program.version(version);
 
-  program.command('login <username>')
-    .description('Add user')
-    .action(login);
+  program.command('add <name>')
+    .description('Add token with alias')
+    .action(add);
 
   program.command('ls')
-    .description('List users')
+    .description('List token aliases')
     .action(ls);
 
-  program.command('use <username>')
-    .description('Use user')
+  program.command('use <alias>')
+    .description('Use token by alias')
     .action(use);
 
   program.parse(argv);
