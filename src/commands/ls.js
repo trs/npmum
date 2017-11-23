a3369519a3d9716f0dec5fed98e49eba0c982933
Tyler Stewart <tylers490@gmail.com>
2017-11-23 16:44:38 -0700

    feat(ls): list current set user

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -7,7 +7,7 @@ const ls = {
   _mapUserTokens
 };
 
-function _mapUserTokens(users) {
+function _mapUserTokens(users, currentUser) {
   return Object.keys(users).map(name => {
     const user = users[name];
     const token = user.token;
@@ -15,21 +15,29 @@ function _mapUserTokens(users) {
 
     return {
       name,
-      token: censoredToken
+      token: censoredToken,
+      current: name === currentUser ? '\u2713' : ''
     };
   });
 }
 
 function handle() {
   const users = storage.getUsers();
+  const currentUser = storage.getCurrentUser();
 
   if (!Object.keys(users).length) {
     console.log('No users added yet!');
     return false;
   }
 
-  const nameData = ls._mapUserTokens(users);
-  const columns = columnify(nameData);
+  const nameData = ls._mapUserTokens(users, currentUser);
+  const columns = columnify(nameData, {
+    config: {
+      current: {
+        align: 'center'
+      }
+    }
+  });
   console.log(columns);
   return true;
 }
